65e661d955fd1306d47ef5e236179e0ba9d8e984
FIRST_NAME LAST_NAME <unusalihr@gmail.com>
2025-05-27 17:41:47 +0200

    all commited

---
+++ test.js
@@ -0,0 +1,14 @@
+console.log(3+3);
+alert("Hello World");
+console.log("Unus");
+
+
+let name="Unus"
+let age=20
+let isStudent=true
+let hobbies=["reading","gaming","coding"]
+let address={
+    city:"New York",
+    console.log(tipeof(address))
+    
+    
